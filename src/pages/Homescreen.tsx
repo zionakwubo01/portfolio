3e0fb4b0374dd8788df4c890f66e0ffb8b3b7399
zionakwubo01 <zionakwubo@gmail.com>
2024-05-16 01:36:10 +0100

    5th

--- src/pages/Homescreen.tsx
+++ src/pages/Homescreen.tsx
@@ -1,10 +1,12 @@
 import Heropage from "./Heropage";
+import Know from "./Know";
 import Skills from "./Skills";
 
 const Homescreen = () => {
   return (
     <div>
       <Heropage />
+      <Know />
       <Skills />
     </div>
   );
---
+++ src/pages/Know.tsx
@@ -0,0 +1,19 @@
+const Know = () => {
+  return (
+    <div className="w-full min-h-[10px] bg-black flex items-center justify-center">
+      <div className="w-[90%] lg:w-[80%] ">
+        <div className=" text-[14px]" style={{ color: "#5E7375" }}>
+          SKILLS --
+        </div>
+        <div
+          className="text-[25px] font-bold lg:text-[40px]"
+          style={{ color: "#BDBDBD" }}
+        >
+          Skills i know
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Know;
